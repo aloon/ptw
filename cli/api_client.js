936cb62465967562a87a6ac5e2a56898f55f871d
Alejandro Gonzalez <alejandro.gonzalez@scmspain.com>
2014-09-17 23:21:30 +0200

    setting message

--- cli/api_client.js
+++ cli/api_client.js
@@ -3,7 +3,6 @@ var request = require('request');
 
 module.exports = {
   getTokenByEmailPass: function (options, callback) {
-
     var url='http://localhost:3000/getToken';
     var data={ form: { email: options.email, pass: options.pass } };
     request.post(url, data, function (error, response, body) {
@@ -13,8 +12,19 @@ module.exports = {
       else{
         callback({'error':'error'})
       }
+    });    
+  },
+  setMessage:function(data, callback){
+    var url='http://localhost:3000/setMessage';
+    var tosend={form:data};
+    request.post(url, data, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        callback(body);
+      }
+      else{
+        callback({'error':'error'})
+      }
     });
-    
   }
 };
 
