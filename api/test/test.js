96c93c56a5a9215851917945e791e08e1b67a379
Alejandro Gonzalez <alejandro.gonzalez@scmspain.com>
2014-09-23 15:19:18 +0200

    test with mocha & chai

--- api/test/test.js
+++ api/test/test.js
@@ -1,9 +1,6 @@
 var assert = require("assert");
-var should = require('should');
-var sinon = require("sinon");
-var rewire = require('rewire');
-var br = rewire('../br');
-//var PubSub = global.PubSub || require("pubsub-js")
+var should = require('chai').should();
+var br = require('../br');
 
 describe('BussinesRules', function(){
   
@@ -19,20 +16,33 @@ describe('BussinesRules', function(){
     before(function(done){
       var db = require('../db');
       db.connect('user',function(col){
-        col.remove({email:'aloon@aloon.com'}, function(err, result){});
+        col.remove({email:'test@test.com'}, function(err, result){});
         done();
       });
     });
     
-    it('no existe usuario con esas caracteristicas', function (done) {
-    
-      br.register({email:'aloon@aloon.com', pass:'1234'}, function(data, error){
-        (true, error==null && data!=null).should.be.true;
+    it('no existe usuario con este email', function (done) {    
+      br.register({email:'test@test.com', pass:'1234'}, function(data, error){
+        (true, error==null && data!=null).should.be.ok;
         done();
       });
-
     });
     
+    it('existe usuario', function (done) {    
+      br.register({email:'test@test.com', pass:'1234'}, function(data, error){
+        console.log((1 == error.errorNum));
+        error.errorNum.should.be.equals(1);
+        done();
+      });
+    });   
+ 
+    after(function(done){
+      var db = require('../db');
+      db.connect('user',function(col){
+        col.remove({email:'test@test.com'}, function(err, result){});
+        done();
+      });
+    });
 
     
   });
