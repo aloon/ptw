708d399c60327d358723f4a89067468089b9f794
Alejandro Gonzalez <alejandro.gonzalez@scmspain.com>
2014-09-24 23:09:45 +0200

    URLAPI

--- cli/api_client.js
+++ cli/api_client.js
@@ -2,8 +2,11 @@ var utils = require('./utils.js');
 var request = require('request');
 
 module.exports = {
+  urlAPI: function(){
+    return 'http://localhost:3000';
+  },
   getTokenByEmailPass: function (options, callback) {
-    var url='http://localhost:3000/getToken';
+    var url = this.urlAPI + '/getToken';
     var data={ form: { email: options.email, pass: options.pass } };
     request.post(url, data, function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -15,7 +18,7 @@ module.exports = {
     });    
   },
   setMessage:function(data, callback){
-    var url='http://localhost:3000/setMessage';
+    var url = this.urlAPI + '/setMessage';
     var tosend={form:data};
     request.post(url, data, function (error, response, body) {
       if (!error && response.statusCode == 200) {
